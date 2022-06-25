0ea9e547d348e4b7929bc48eebd79980b445450e
Nicolas Le Cam <nicolas.lecam@digital4better.com>
2022-06-25 13:32:39 +0200

    Remove unused const

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,11 +1,5 @@
 import type { Handle } from "@sveltejs/kit";
 
-const NETLIFY_LOCAL_DEV_CONFIG = `
-backend:
-  name: git-gateway
-local_backend: true
-`;
-
 export const handle: Handle = async ({ event, resolve }) => {
   if (event.url.pathname === "/admin" || event.url.pathname === "/admin/") {
     return Response.redirect(new URL("/admin/index.html", event.url));
