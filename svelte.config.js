dcd5c6170d624d951eb3f36a9e6618690fddaf30
Nicolas Le Cam <nicolas.lecam@digital4better.com>
2022-06-25 13:34:21 +0200

    Fix problems in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,13 +5,13 @@ import mdsvexConfig from "./mdsvex.config.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
+  extensions: [".svelte", ...(mdsvexConfig.extensions ?? [])],
 
   kit: {
     adapter: adapter(),
 
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/laplie" : "",
+      base: process.env["NODE_ENV"] === "production" ? "/laplie" : "",
     },
 
     prerender: {
