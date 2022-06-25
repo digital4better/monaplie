2eccf94f7234ee7ec64d62ccd6be32b5642279ab
Nicolas Le Cam <nicolas.lecam@digital4better.com>
2022-06-25 16:59:35 +0200

    Use the same base path on local and production env to avoid breaking one or both of them

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
     adapter: adapter(),
 
     paths: {
-      base: process.env["NODE_ENV"] === "production" ? "/laplie" : "",
+      base: "/laplie",
     },
 
     prerender: {
