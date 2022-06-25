6e753d5075c4630840f63b8496770a91f227df72
Nicolas Le Cam <nicolas.lecam@digital4better.com>
2022-06-25 13:34:21 +0200

    Fix problems in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
     adapter: adapter(),
 
     paths: {
-      base: "/laplie",
+      base: process.env["NODE_ENV"] === "production" ? "/laplie" : "",
     },
 
     prerender: {
