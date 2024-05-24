8f17583166f5db825c1fb8e92e71193e3a53a1fd
Damien Lecan <damien.lecan@digital4better.com>
2024-05-24 16:54:14 +0200

    Revert "Revert "chores(analytics): replace Tarteaucitron by raw GTM""
    This reverts commit 695f3ffa8fe59c36e2efb827dd621360b8aaac24.

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,4 +1,12 @@
 <svelte:head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-559S4SD7');</script>
+  <!-- End Google Tag Manager -->
+  <!--
   <script
     src="https://cdn.jsdelivr.net/npm/tarteaucitronjs@1/tarteaucitron.min.js"
   ></script>
@@ -10,6 +18,7 @@
     tarteaucitron.user.googletagmanagerId = "GTM-559S4SD7";
     (tarteaucitron.job = tarteaucitron.job || []).push("googletagmanager");
   </script>
+  -->
 </svelte:head>
 
 <style global>
